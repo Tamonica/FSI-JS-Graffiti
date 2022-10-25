f1d1aca281e8ab3368fac8e084a7d03ad02670e1
Tamonica Weathers <tamonica@Tamonicas-MBP.attlocal.net>
2022-10-25 19:41:21 -0400

    style changes

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header');
+header.style.textAlign = 'left'
+
+const dogImages = document.querySelectorAll('.dog-image');
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = '10px';
+}
+ 
+
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.float = "right";
+
+}
+
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderStyle = "inset"
+
+}
+
+let footer = document.querySelector('.footer')
+footer.style.backgroundColor = 'beige'
+
